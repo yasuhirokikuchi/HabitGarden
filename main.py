e72bdc9330932859f638bb31e850440551093ef9
kikuchi <natumetarou@gmail.com>
2025-11-21 09:38:45 +0900

    画像の追加

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 import streamlit as st
 
-st.title("はじめてのStreamlitアプリ")
-st.write("こんにちは、Streamlitの世界へようこそ！")
+image_title = "image/title/unnamed.png"
+
+# テキスト(マークダウンで書けます。)
+st.write("# title")
+
+# 注釈ｗｗｗ
+st.caption("注釈")
+
+# 画像
+st.image(image_title)
+
+# テーブル
+import pandas as pd
+df = pd.DataFrame(
+        {
+            "first column": [1, 2, 3, 4],
+            "second column": [10, 20, 30, 40],
+        }
+    )
+st.write(df)
+
+# チャート
+st.line_chart(df)
