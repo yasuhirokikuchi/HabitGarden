aa8cbb99500bfacd945f032feedc093cb93fc09d
kd1430535@kobedenshi078.onmicrosoft.com <kd1430535@kobedenshi.ac.jp>
2025-12-08 10:54:24 +0900

    A

--- main.py
+++ main.py
@@ -20,7 +20,18 @@ LEVEL_DATA = {
 # 説明
 # =========================
 def render_explanation():
-    
+    import os
+import streamlit as st
+
+st.set_page_config(page_title="Habit Garden", page_icon=":seedling:")
+
+with st.container(horizontal=True, horizontal_alignment="center"):
+
+    st.image("Image_20251201_111815_891.jpeg", width=500)
+
+    st.markdown('<h1 style="text-align:center;">🌿ようこそ <span style="color:green;">Habit Garden</span> へ</h1>',unsafe_allow_html=True)
+
+    st.subheader("Habit Gardenでは決めた目標を植物の成長度合いで表し、目標の達成が一目でわかるようになるアプリ")
 
 
 # =========================
