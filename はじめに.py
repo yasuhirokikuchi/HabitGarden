822a4d2be9caa8ae660da93b112850d464b53cf3
kd1430535@kobedenshi078.onmicrosoft.com <kd1430535@kobedenshi.ac.jp>
2025-12-08 10:39:27 +0900

    A

--- はじめに.py
+++ はじめに.py
@@ -1,12 +1,18 @@
 import streamlit as st
 
+import os
+import streamlit as st
+
 st.set_page_config(page_title="Habit Garden", page_icon=":seedling:")
 
 with st.container(horizontal=True, horizontal_alignment="center"):
 
-    st.title("🌱ようこそHabit Gardenへ")
-    st.image("image/title/unnamed.png")
+    st.image("Image_20251201_111815_891.jpeg", width=500)
 
-    st.subheader("Habit Gardenでは目標を植物の成長度合いで可視化し、目標の進捗が一目でわかるようになるアプリです。")
+    st.markdown('<h1 style="text-align:center;">🌿ようこそ <span style="color:green;">Habit Garden</span> へ</h1>',unsafe_allow_html=True)
+
+    st.subheader("Habit Gardenでは決めた目標を植物の成長度合いで表し、目標の達成が一目でわかるようになるアプリ")
+
+
 
     
