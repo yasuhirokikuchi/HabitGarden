bb4f4972e6ffdcb40890b787c5dbcdac00b89b35
kikuchi <natumetarou@gmail.com>
2026-01-14 12:04:24 +0900

    a

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ LEVEL_DATA = {
 def login_page():
     with st.container(horizontal=True, horizontal_alignment="center"):
 
-        st.image("images/title/title.jpeg", width=500)
+        st.image("images/title/title.jpeg", width=400)
         st.markdown('<h1 style="text-align:center;">🌿ようこそ <span style="color:green;">Habit Garden</span> へ</h1>',unsafe_allow_html=True)
         st.subheader("Habit Gardenでは決めた目標を植物の成長度合いで表し、目標の達成が一目でわかるようになるアプリ")
 
